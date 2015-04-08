9b2a7641c32f2452ee4b0b7b7148eae5dd7da750
Kyle <kyleseto@hotmail.com>
2015-04-08 10:07:31 +1000

    Fixed a few poor coding syntax errors.
    You should always test scripts before running them on production servers.

--- sysadminadd.sh
+++ sysadminadd.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 #######################################################################################################################
-# Version 1.0
+# Version 1.1
 #
 # Created by kyle 
 #
@@ -15,7 +15,7 @@
 #######################################################################################################################
 
 #email string for the domain of the account
-email=<company.org> 
+email=<company.org>
 #default shell you want for users
 BASH_LOC="/bin/bash"
 #default group users will be placed in
@@ -45,36 +45,36 @@ if [ $EUID -eq 0 ]; then
 
 		#Gets username and homedir
 		username=${USERS[$COUNT]}
-
+		homedir="/home/$username"
 		# Check if the account already exists
-		if id -u $username >/dev/null 2>&1 ; then
+		if id -u "$username" >/dev/null 2>&1 ; then
 			echo "$username:Already exists"
 		else
 			# Create account for those that don't exist
-			homedir="/home/$username"
-			echo $username is being created 
+			echo "$username" is being created 
 			# Create a random password
-			PASS=`openssl rand -base64 12 | cut -c 1-10`
+			PASS=$(openssl rand -base64 12 | cut -c 1-10)
 			# Create the account 
-			useradd -c "${NAME[$COUNT]}" -s $BASH_LOC -u ${UIDS[$COUNT]} -g $GROUP -d $homedir -G adm $username
-			echo $PASS
+			useradd -c "${NAME[$COUNT]}" -s "$BASH_LOC" -u "${UIDS[$COUNT]}" -g "$GROUP" -d "$homedir" -G sys "$username"
+			echo "$PASS"
 			# Set password for the account
-			echo $PASS | passwd $username  --stdin
-			chage -d 0 $username
+			echo "$PASS" | passwd "$username"  --stdin
+			chage -d 0 "$username"
 			# Set the flag to say password has been set for the account
 			passbool="1"
 		fi
-		if [ -f $username.pub ] ; then
+		if [ -f "$username".pub ] ; then
 			#Make the directory for the users .ssh profile
-			mkdir -p $homedir/.ssh/
-			chmod 700 $homedir/
+			mkdir -p "$homedir"/.ssh/
+			chmod -v 700 "$homedir"/
 			#Check the users has a public key to be placed on the machine
 			#force copy the key to their ssh profile
 			echo "$username:Public key is being copied"
-			cat $username.pub >> $homedir/.ssh/authorized_keys
-			chmod 600 $homedir/.ssh/authorized_keys
-			chown $username $homedir/
-			chown $username $homedir/.ssh/authorized_keys
+			cat "$username".pub >> "$homedir"/.ssh/authorized_keys
+			chmod -v 600 "$homedir"/.ssh/authorized_keys
+			chown -v "$username" "$homedir"/
+			chown -v "$username" "$homedir"/.ssh/
+			chown -v "$username" "$homedir"/.ssh/authorized_keys
 			#Test if it worked
 			echo "$username:Key copied correctly"
 			sshbool="1"
@@ -90,7 +90,7 @@ if [ $EUID -eq 0 ]; then
 					echo "Subject:Account Created on $(hostname -f) with tmp password $PASS and ssh key copied"
 					echo "From:root@$(hostname -f)"
 					echo "Your temporary password is $PASS . Your ssh public key has also been added."
-				)  | sendmail $username@$email ;
+				)  | sendmail "$username"@"$email" ;
 				echo "$username:Account created and ssh key copied"
 			else
 				# Emails users that their account has been created
@@ -98,7 +98,7 @@ if [ $EUID -eq 0 ]; then
 					echo "Subject:Account Created on $(hostname -f) with temp password of $PASS"
 					echo "From:root@$(hostname -f)"
 					echo "Your temporary password is $PASS."
-				)  | sendmail $username@$email ;
+				)  | sendmail "$username"@"$email" ;
 				echo "$username:Account created with tmp password"
 			fi
 		elif [ $sshbool -eq "1" ] ; then
@@ -107,17 +107,17 @@ if [ $EUID -eq 0 ]; then
 				echo "Subject:Your ssh key has been copied to $(hostname -f) for your existing account"
 				echo "From:root@$(hostname -f)"
 				echo "Your account already exists on$(hostname -f) so we copied your ssh key into your profile"
-			)  | sendmail $username@$email ;
+			)  | sendmail "$username"@"$email" ;
 			echo "$username:Account already existed copied ssh key"
 		else
 			echo "$username:Account not created and key not copied. No email sent."
 		fi
-		COUNT=$(expr $COUNT + 1 )
+		COUNT=$($COUNT + 1 )
 	done 
 
 	# Append the sys group to the Sudoers file
-	echo "%adm	ALL=(ALL)	ALL" >> /etc/sudoers
-	echo "adm Group has been appended to sudoers file"
+	echo "%sys	ALL=(ALL)	ALL" >> /etc/sudoers
+	echo "sys Group has been appended to sudoers file"
 
 	#Lock old accounts
 	echo " "
@@ -129,15 +129,15 @@ if [ $EUID -eq 0 ]; then
 		#Gets username
 		lockuser=${LOCKUSERS[$COUNT]}
 		#Checks if the account exists
-		if id -u $lockuser >/dev/null 2>&1; then
+		if id -u "$lockuser" >/dev/null 2>&1; then
 			echo "$lockuser:Locking account"
 			# Lock the account
-			passwd $lockuser -l
+			passwd "$lockuser" -l
 		else 
 			echo "   $lockuser:Does not exist"
 		fi
 		# loops through users
-		COUNT=$(expr $COUNT + 1 )
+		COUNT=$($COUNT + 1)
 	done
 else
 	# Script need root permissions
